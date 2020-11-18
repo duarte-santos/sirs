6f0b63e047979a698b3dad16ec3b54c0a4ecf63e
duarte-santos <duarte.dos.santos@tecnico.ulisboa.pt>
2020-11-18 23:41:11 +0000

    base client

--- README.md
+++ README.md
@@ -4,6 +4,15 @@ Segurança Informática em Redes e Sistemas - IST - 2020/2021
 ## Server
 To run the server:
  ```sh
+ $ cd server
  $ mvn clean install
  $ mvn exec:java -Dexec.args="31337"
+ ```
+
+## Client
+To run the client:
+ ```sh
+ $ cd client
+ $ mvn clean install
+ $ mvn exec:java -Dexec.args="localhost 31337"
  ```
--- client/src/pt/tecnico/contacttracing/client/Client.java
+++ client/src/pt/tecnico/contacttracing/client/Client.java
@@ -1,8 +1,7 @@
-package pt.tecnico.contacttracing.server.client;
+package pt.tecnico.contacttracing.client;
 
 import java.io.*;
 import java.net.*;
-import java.util.*;
 
 import com.google.gson.*;
 
@@ -12,6 +11,8 @@ public class Client {
 	private static final int BUFFER_SIZE = 65_507;
 
 	public static void main(String[] args) throws IOException {
+		System.out.println();
+
 		// Check arguments
 		if (args.length < 2) {
 			System.err.println("Argument(s) missing!");
@@ -41,26 +42,24 @@ public class Client {
 			String keyText = "123456789";
 			requestJson.addProperty("key", keyText);
 		}
-		System.out.println("Request message: " + requestJson);
+		System.out.println("> Request message: " + requestJson);
 
 		// Send request
 		byte[] clientData = requestJson.toString().getBytes();
-		System.out.printf("%d bytes %n", clientData.length);
 		DatagramPacket clientPacket = new DatagramPacket(clientData, clientData.length, serverAddress, serverPort);
 		socket.send(clientPacket);
-		System.out.printf("Request packet sent to %s:%d!%n", serverAddress, serverPort);
+		System.out.printf("> Sent packet to %s:%d (%d bytes)%n", serverAddress, serverPort, clientData.length);
 
 		// Receive response
 		byte[] serverData = new byte[BUFFER_SIZE];
 		DatagramPacket serverPacket = new DatagramPacket(serverData, serverData.length);
-		System.out.println("Wait for response packet...");
+		System.out.println("> Wait for response packet...");
 		socket.receive(serverPacket);
-		System.out.printf("Received packet from %s:%d!%n", serverPacket.getAddress(), serverPacket.getPort());
-		System.out.printf("%d bytes %n", serverPacket.getLength());
+		System.out.printf("> Received packet from %s:%d (%d bytes)%n", serverPacket.getAddress(), serverPacket.getPort(), serverPacket.getLength());
 
 		// Convert response to string
 		String serverText = new String(serverPacket.getData(), 0, serverPacket.getLength());
-		System.out.println("Received response: " + serverText);
+		System.out.println("> Received response: " + serverText);
 
 		/* Json:
 			{
@@ -74,11 +73,11 @@ public class Client {
 		{
 			status = responseJson.get("status").getAsString();
 		}
-		System.out.printf("Message from '%s':%n%s%n", status);
+		//System.out.printf("Message from '%s':%n%s%n", status);
 
 		// Close socket
 		socket.close();
-		System.out.println("Socket closed");
+		System.out.println("> Socket closed");
 	}
 
 }
