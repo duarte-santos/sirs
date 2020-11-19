9605b41d7e4cb7cd81d9c7ce914e78ea738fd3b8
Sofitch <sofitch06@gmail.com>
2020-11-19 20:14:07 +0000

    Server storage is now concurrent

--- server/src/main/java/pt/tecnico/contacttracing/server/Storage.java
+++ server/src/main/java/pt/tecnico/contacttracing/server/Storage.java
@@ -12,6 +12,10 @@ import java.net.*;
 import com.google.gson.*;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.Map;
+import java.util.Set;
 import java.time.Instant;
 
 
@@ -20,12 +24,10 @@ public class Storage {
 	public class InfectedData {
 		private int _numbers;
 		private int _keys;
-		private Instant _date;
 
-		InfectedData(int numbers, int keys, Instant date) {
+		InfectedData(int numbers, int keys) {
 			_numbers = numbers;
 			_keys = keys;
-			_date = date;
 		}
 
 		public int getNumbers(){
@@ -35,32 +37,33 @@ public class Storage {
 		public int getKeys(){
 			return _keys;
 		}
-
-		public Instant getTimestamp(){
-			return _date;
-		}
     }
-    
-    private List<InfectedData> _storage = new ArrayList<InfectedData>();
 
-	public List<InfectedData> getInfectedData() {
+	private Map<Instant, InfectedData> _storage = new ConcurrentHashMap<Instant, InfectedData>();
+
+	public Map<Instant, InfectedData> getInfectedData() {
 		return _storage;
 	}
-	public void addInfectedData(InfectedData data) {
-		_storage.add(data);
+	public void addInfectedData(Instant instant, InfectedData data) {
+		_storage.put(instant, data);
 	}
 
-	public void storeInfectedData(int number, int key, Instant timestamp) {
-		InfectedData data = new InfectedData(number, key, timestamp);
-		addInfectedData(data);
+	public void storeInfectedData(int number, int key, Instant instant) {
+		InfectedData data = new InfectedData(number, key);
+		addInfectedData(instant, data);
 	}
 
 	public List<InfectedData> getUpdates(Instant lastUpdate) {
-		List<InfectedData> all_data = getInfectedData();
+		Map<Instant, InfectedData> all_data = getInfectedData();
+		Set<Instant> all_ts = all_data.keySet();
+
 		List<InfectedData> new_data = new ArrayList<InfectedData>();
 
-		for (InfectedData data : all_data)
-			if (data.getTimestamp().compareTo(lastUpdate) >= 0) new_data.add(data);
+		for (Instant ts : all_ts)
+			if (ts.compareTo(lastUpdate) >= 0) {
+				InfectedData relevant_data = all_data.get(ts);
+				new_data.add(relevant_data);
+			}
 
 		return new_data;
 	}
