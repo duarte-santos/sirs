db27c554f0c7ae034aa6d16bfcd65b7e7a2c4291
duarte-santos <duarte.coelho.santos@gmail.com>
2020-12-08 01:30:10 +0000

    bluetooth scan

--- PooPooRoo/app/src/main/java/poo/poo/roo/MainActivity.java
+++ PooPooRoo/app/src/main/java/poo/poo/roo/MainActivity.java
@@ -1,5 +1,6 @@
 package poo.poo.roo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -7,147 +8,77 @@ import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.le.BluetoothLeScanner;
-import android.bluetooth.le.ScanCallback;
-import android.bluetooth.le.ScanFilter;
-import android.bluetooth.le.ScanResult;
-import android.bluetooth.le.ScanSettings;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.ParcelUuid;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
-import java.util.ArrayList;
-import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    private BluetoothAdapter mBluetoothAdapter  = null;
-    private BluetoothLeScanner mBluetoothLeScanner = null;
+    private static final String TAG = MainActivity.class.getSimpleName();
 
-    public static final int REQUEST_BT_PERMISSIONS = 0;
-    public static final int REQUEST_BT_ENABLE = 1;
-    public static final int MY_PERMISSIONS_REQUEST_LOCATION = 99;
-    public static final ParcelUuid Service_UUID = ParcelUuid.fromString("0000b81d-0000-1000-8000-00805f9b34fb");
+    private boolean _Scanning = false;
+    private Scanner _bleScanner;
 
-    private boolean mScanning = false;
-    private Handler mHandler = null;
-
-    private Button btnScan = null;
-
-    private boolean locationPermission = false;
-
-    private ScanCallback mLeScanCallback =
-            new ScanCallback() {
-
-                @Override
-                public void onScanResult(int callbackType, final ScanResult result) {
-                    //super.onScanResult(callbackType, result);
-                    if (result.getDevice().getName() == null)
-                        Log.i("BLE", result.getDevice().getAddress());
-                    else {
-                        Log.i("BLE", result.getDevice().getName());
-                    }
-
-                }
-
-                @Override
-                public void onScanFailed(int errorCode) {
-                    super.onScanFailed(errorCode);
-                    Log.i("BLE", "error");
-                }
-            };
-
-
+    private Button _ScanButton;
+    //private Button _AdvertiseButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("Contact Tracing");
 
-        btnScan = (Button) findViewById(R.id.btnScan);
+        _ScanButton = (Button) findViewById(R.id.scan_btn);
+        //_AdvertiseButton = (Button) findViewById(R.id.advertise_btn);
 
-        this.mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-        this.mBluetoothLeScanner = mBluetoothAdapter.getBluetoothLeScanner();
-        this.mHandler = new Handler();
+        _ScanButton.setOnClickListener(this);
+        //_AdvertiseButton.setOnClickListener(this);
 
-        checkBtPermissions();
-        checkLocationPermission();
-        enableBt();
+        if (savedInstanceState == null) {
+            BluetoothAdapter btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+            checkLocationPermission();
+            checkBluetoothSupport(btAdapter);
+
+            _bleScanner = new Scanner(this, btAdapter);
+        }
     }
 
-    public void onBtnScan(View v){
-        if (mScanning){
-            mScanning = false;
-            scanLeDevice(false);
-            btnScan.setText("SCAN");
+
+    /* ====================================================================== */
+    /* ====[                        PERMISSIONS                         ]==== */
+    /* ====================================================================== */
+
+    private void checkBluetoothSupport(BluetoothAdapter btAdapter) {
+        // Is Bluetooth supported on this device?
+        if (btAdapter != null) {
+            // Is Bluetooth turned on?
+            if (btAdapter.isEnabled()) {
+                // Are Bluetooth Advertisements supported on this device?
+                if (btAdapter.isMultipleAdvertisementSupported()) {
+                    // Everything is supported and enabled.
+
+                } else {
+                    // Bluetooth Advertisements are not supported.
+                    AlertAndExit(R.string.bt_required, R.string.bt_ads_not_supported);
+                }
+            } else {
+                // Prompt user to turn on Bluetooth.
+                Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                startActivityForResult(enableBtIntent, Constants.REQUEST_ENABLE_BT);
+            }
         } else {
-            mScanning = true;
-            scanLeDevice(true);
-            btnScan.setText("STOP");
+            // Bluetooth is not supported.
+            AlertAndExit(R.string.bt_required, R.string.bt_not_supported);
         }
     }
 
-    public void checkBtPermissions() {
-        this.requestPermissions(
-                new String[]{
-                        Manifest.permission.BLUETOOTH, Manifest.permission.BLUETOOTH_ADMIN
-                },
-                REQUEST_BT_PERMISSIONS);
-    }
-
-    public void enableBt(){
-        if (mBluetoothAdapter == null) {
-            // Device does not support Bluetooth
-        }
-        if (!mBluetoothAdapter.isEnabled()) {
-            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-            startActivityForResult(enableBtIntent, REQUEST_BT_ENABLE);
-        }
-    }
-
-    public void scanLeDevice(final boolean enable) {
-        //ScanSettings mScanSettings = new ScanSettings.Builder().setScanMode(ScanSettings.SCAN_MODE_BALANCED).setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES).build();
-
-        if (enable) {
-            mScanning = true;
-            Log.i("Scanning", "start");
-            mBluetoothLeScanner.startScan(buildScanFilters(), buildScanSettings(), mLeScanCallback);
-        } else {
-            Log.i("Scanning", "stop");
-            mScanning = false;
-            mBluetoothLeScanner.stopScan(mLeScanCallback);
-        }
-    }
-
-    private List<ScanFilter> buildScanFilters() {
-        List<ScanFilter> scanFilters = new ArrayList<>();
-        ScanFilter filter = new ScanFilter.Builder().setServiceUuid(Service_UUID).build();
-        scanFilters.add(filter);
-
-        return scanFilters;
-    }
-
-    private ScanSettings buildScanSettings() {
-        ScanSettings settings = new ScanSettings.Builder()
-                    .setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
-                    .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
-                    .setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
-                    .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT)
-                    .setReportDelay(0L)
-                    .build();
-        return settings;
-    }
-
-    public boolean checkLocationPermission() {
-        if (ContextCompat.checkSelfPermission(this,
-                Manifest.permission.ACCESS_COARSE_LOCATION)
-                != PackageManager.PERMISSION_GRANTED) {
+    public void checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
 
             // Show an explanation to the user *asynchronously* -- don't block
             // this thread waiting for the user's response! After the user
@@ -159,44 +90,69 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public void onClick(DialogInterface dialogInterface, int i) {
                         //Prompt the user once explanation has been shown
-                        ActivityCompat.requestPermissions(MainActivity.this,
-                                new String[]{Manifest.permission.ACCESS_COARSE_LOCATION},
-                                MY_PERMISSIONS_REQUEST_LOCATION);
+                        ActivityCompat.requestPermissions(MainActivity.this, new String[]{ Manifest.permission.ACCESS_COARSE_LOCATION }, Constants.REQUEST_LOCATION);
                     }
                 })
                 .create()
                 .show();
-            return true;
-        } else {
-            return true;
         }
     }
 
     @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_LOCATION : {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-
-                    // permission was granted, yay! Do the
-                    // location-related task you need to do.
-                    if (ContextCompat.checkSelfPermission(this,
-                            Manifest.permission.ACCESS_COARSE_LOCATION)
-                            == PackageManager.PERMISSION_GRANTED) {
-                        locationPermission = true;
-                    }
-
-                } else {
-                    locationPermission = false;
-
-                    // permission denied, boo! Disable the
-                    // functionality that depends on this permission.
-
-                }
-                locationPermission = false;
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == Constants.REQUEST_LOCATION) {
+            // If request is cancelled, the result arrays are empty.
+            if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                // permission denied, boo! Disable the functionality that depends on this permission.
+                AlertAndExit(R.string.title_location_denied, R.string.title_location_denied);
             }
-
         }
     }
-}
+
+
+    /* ====================================================================== */
+    /* ====[                      UI INTERACTION                        ]==== */
+    /* ====================================================================== */
+
+    public void scan(View v){
+        if (_Scanning){
+            _Scanning = false;
+            _bleScanner.stopScanning();
+            _ScanButton.setText(R.string.bt_scan);
+        } else {
+            _Scanning = true;
+            _bleScanner.startScanning();
+            _ScanButton.setText(R.string.bt_stop);
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        if( v.getId() == R.id.scan_btn) {
+            scan(v);
+        }
+        //else if( v.getId() == R.id.advertise_btn ) {
+        //    advertise();
+        //}
+    }
+
+
+    /* ====================================================================== */
+    /* ====[                        AUXILIARY                           ]==== */
+    /* ====================================================================== */
+
+    private void AlertAndExit(int title, int message) {
+        new AlertDialog.Builder(this)
+                .setTitle(title)
+                .setMessage(message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .create()
+                .show();
+    }
+
+} // class MainActivity
--- PooPooRoo/app/src/main/java/poo/poo/roo/Scanner.java
+++ PooPooRoo/app/src/main/java/poo/poo/roo/Scanner.java
@@ -1,4 +1,102 @@
 package poo.poo.roo;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
+import android.content.Context;
+import android.util.Log;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.List;
+
+
+/**
+ * Scans for Bluetooth Low Energy Advertisements matching a filter and displays them to the user.
+ */
 public class Scanner {
-}
+
+    private static final String TAG = Scanner.class.getSimpleName();
+
+    private final Context _context;
+    private final BluetoothLeScanner _BluetoothLeScanner;
+    private ScanCallback _ScanCallback;
+
+    public Scanner(Context context, BluetoothAdapter btAdapter) {
+        _context = context;
+        _BluetoothLeScanner = btAdapter.getBluetoothLeScanner();
+    }
+
+    /**
+     * Start scanning for BLE Advertisements.
+     */
+    public void startScanning() {
+        Log.d(TAG, "Starting Scanning");
+
+        // Kick off a new scan.
+        _ScanCallback = new SampleScanCallback();
+        _BluetoothLeScanner.startScan(buildScanFilters(), buildScanSettings(), _ScanCallback);
+    }
+
+    /**
+     * Stop scanning for BLE Advertisements.
+     */
+    public void stopScanning() {
+        Log.d(TAG, "Stopping Scanning");
+
+        // Stop the scan, wipe the callback.
+        _BluetoothLeScanner.stopScan(_ScanCallback);
+        _ScanCallback = null;
+    }
+
+    /**
+     * Return a List of {@link ScanFilter} objects to filter by Service UUID.
+     */
+    private List<ScanFilter> buildScanFilters() {
+        List<ScanFilter> scanFilters = new ArrayList<>();
+
+        ScanFilter.Builder builder = new ScanFilter.Builder();
+        // Comment out the below line to see all BLE devices around you
+        //builder.setServiceUuid(Constants.Service_UUID);
+        scanFilters.add(builder.build());
+
+        return scanFilters;
+    }
+
+    /**
+     * Return a {@link ScanSettings} object set to use low power (to preserve battery life).
+     */
+    private ScanSettings buildScanSettings() {
+        ScanSettings.Builder builder = new ScanSettings.Builder();
+        builder.setScanMode(ScanSettings.SCAN_MODE_LOW_POWER)
+                .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
+                .setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
+                .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT)
+                .setReportDelay(0L);
+        return builder.build();
+    }
+
+    private class SampleScanCallback extends ScanCallback {
+
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            //super.onScanResult(callbackType, result);
+
+            String name = result.getDevice().getName();
+            String address = result.getDevice().getAddress();
+            String msg = ( ( name != null ) ? name : address );
+            Log.i(TAG, msg);
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            super.onScanFailed(errorCode);
+            Toast.makeText(_context, "Scan failed with error: " + errorCode, Toast.LENGTH_LONG).show();
+        }
+
+    } // class SampleScannerCallback
+
+} // class Scanner
